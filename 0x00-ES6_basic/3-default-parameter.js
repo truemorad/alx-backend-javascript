f66ab67010befc33fd471f26aecf6e5462281ba1
truemorad <truemorad@gmail.com>
2024-09-30 15:21:59 +0400

    autocommit

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,5 +1,5 @@
 export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  if (expansion1989 === undefined) => { expansion1989 = 89 };
-  if (expansion2019 === undefined) => { expansion2019 = 19 };
+  if (expansion1989 === undefined) => 89;
+  if (expansion2019 === undefined) => 19;
   return initialNumber + expansion1989 + expansion2019;
 }
