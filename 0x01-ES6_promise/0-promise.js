10165ec471c78e23d19df6ab98ad637d1466b397
truemorad <truemorad@gmail.com>
2024-10-04 18:47:25 +0400

    eslint-check

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,9 @@
 function getResponseFromAPI() {
-  const success = true
-    if (success) {
-      return Promise.resolve('success');
-    }
-    return Promise.reject(new Error ('failed'));
+  const success = true;
+  if (success) {
+    return Promise.resolve('success');
+  }
+  return Promise.reject(new Error('failed'));
 }
 
-  export default getResponseFromAPI;
+export default getResponseFromAPI;
