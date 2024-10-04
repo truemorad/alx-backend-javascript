c27c58d395da8fc4c5b2fd9072962508f817e785
truemorad <truemorad@gmail.com>
2024-10-04 19:17:25 +0400

    fixes

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,8 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    const resolved = { status:200, body: 'success', };
     if (success) {
-      return resolve(resolved);
-    } else {
-      return reject(new Error('The fake API is not working currently'));
+      return resolve({ status: 200, body: 'success' });
     }
+    return reject(new Error('The fake API is not working currently'));
   });
 }
