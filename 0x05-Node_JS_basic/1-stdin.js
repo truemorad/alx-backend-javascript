1cd660765bd201013dc37aebe5d653d4413d4b38
truemorad <truemorad@gmail.com>
2024-11-27 01:05:43 +0400

    fixed-eslint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,5 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-process.stdin.on('data', name => { 
-    name = name.toString()
-    process.stdout.write(`Your name is: ${name}`);
-    process.exit()
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
+  process.exit();
 });
