6e114ebb274425388b912ebbe599eece98d4e6dd
truemorad <truemorad@gmail.com>
2024-12-10 14:22:45 +0400

    *maybeThisTime

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -4,40 +4,40 @@ export default class ALXCourse {
     this._length = length;
     this._students = students;
   }
-
-  // getters
+  // name
+  // getter
   get name() {
     return this._name;
   }
-
+  // setter
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+  // length
+  // getter
   get length() {
     return this._length;
   }
-
+  // setter
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+  // students
+  // getter
   get students() {
     return this._students;
   }
-
-  // setters
-  set name(new1) {
-    if (typeof new1 !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-    this._name = new1;
-  }
-
-  set length(new2) {
-    if (typeof new2 !== 'number') {
-        throw new TypeError('Length must be a number');
-      }
-    this._name = new2;
-  }
-
-  set students(new3) {
-    if (new3 instanceof Array) {
-        this._students = students;
-      } else {
-        throw new TypeError('Students must be an Array of strings');
-      }
+  // setter
+  set students(students) {
+    if (students instanceof Array) {
+      this._students = students;
+    }
+    throw new TypeError('Students must be an Array of strings');
   }
 }
