58f45032d1fda148d3049997d5bed94039d41538
truemorad <truemorad@gmail.com>
2024-12-10 14:07:00 +0400

    *maybeThisIsHowTo

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -20,14 +20,24 @@ export default class ALXCourse {
 
   // setters
   set name(new1) {
+    if (typeof new1 !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
     this._name = new1;
   }
 
   set length(new2) {
+    if (typeof new2 !== 'number') {
+        throw new TypeError('Length must be a number');
+      }
     this._name = new2;
   }
 
   set students(new3) {
-    this._name = new3;
+    if (new3 instanceof Array) {
+        this._students = students;
+      } else {
+        throw new TypeError('Students must be an Array of strings');
+      }
   }
 }
